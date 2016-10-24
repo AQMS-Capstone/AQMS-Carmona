984922dafbbfedb46fe13361d14abb88b224ae59
Lyle Vince Dela Cuesta <lylevincedelacuesta@gmail.com>
2016-10-25 02:16:27 +0800

    More dynamic ! YEY! Easy to cuztomize card! YEY!

--- js/init.js
+++ js/init.js
@@ -1,4 +1,33 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
-    
+    $('.carousel.carousel-slider').carousel({full_width: true});
+
+
 })
+
+var optionIsVisible = true;
+
+$( "#nextItem" ).click(function() {
+    $('.carousel').carousel('next');
+
+    if(optionIsVisible){
+        $( "#plotOption" ).hide();
+        optionIsVisible = false;
+    }
+    else{
+        $( "#plotOption" ).show();
+        optionIsVisible = true;
+    }
+});
+
+$( "#prevItem" ).click(function() {
+    $('.carousel').carousel('prev');
+    if(optionIsVisible){
+        $( "#plotOption" ).hide();
+        optionIsVisible = false;
+    }
+    else{
+        $( "#plotOption" ).show();
+        optionIsVisible = true;
+    }
+});
--- public/home.php
+++ public/home.php
@@ -25,67 +25,135 @@
 </head>
 
 <body>
-
-<div>
-    <nav class="z-depth-0">
-        <div class="nav-wrapper">
-            <a href="#"><img class="brand-logo center" src="../res/logo.png"> </a>
-        </div>
-    </nav>
-</div>
-
-<div>
-    <nav class="z-depth-0">
-        <div class="nav-wrapper">
-            <ul id="nav-mobile" class="centered-nav hide-on-med-and-down">
-                <li><a href="home.php"><span class="material-icons">home</span> Home</a></li>
-                <li><a class="dropdown-button" href="#!" data-activates="dropdown1"><span class="material-icons">location_on</span> Sensors Map<i class="material-icons right">arrow_drop_down</i></a></li>
-                <li><a href="data-and-reports.html"><span class="material-icons">trending_up</span> Data and Reports</a></li>
-                <li><a href="about.html"><span class="material-icons">email</span> About Us</a></li>
-            </ul>
-        </div>
-    </nav>
-</div>
-<!-- Dropdown Structure -->
-<ul id="dropdown1" class="dropdown-content">
-    <li><a id="drpBancal">SLEX - Carmona Exit</a></li>
-    <li><a id="drpSLEX">Bancal</a></li>
-</ul>
-<div>
-    <nav>
-        <div class="nav-wrapper white">
-            <ul id="nav-mobile" class="centered-nav nav-status hide-on-med-and-down black-text">
-                <li><a style="color: #2196F3;"><span class="material-icons">cloud</span> Good</a></li>
-                <li><a style="color: #FFEB3B;"><span class="material-icons">cloud</span> Moderate</a></li>
-                <li><a style="color: #FF9800;"><span class="material-icons">cloud</span> Unhealthy for Sensitive Groups</a></li>
-                <li><a style="color: #f44336;"><span class="material-icons">cloud</span> Unhealthy</a></li>
-                <li><a style="color: #9C27B0;"><span class="material-icons">cloud</span> Very Unhealthy</a></li>
-                <li><a style="color: #b71c1c;"><span class="material-icons">cloud</span> Hazardous</a></li>
-            </ul>
-        </div>
-    </nav>
-</div>
-
-<div class="row float">
-    <div class="col s12 m6">
-        <div class="card blue-grey darken-1">
-            <div class="card-content white-text">
-                <span class="card-title">Card Title</span>
-                <p>I am a very simple card. I am good at containing small bits of information.
-                    I am convenient because I require little markup to use effectively.</p>
+<div class="header">
+    <div>
+        <nav class="z-depth-0">
+            <div class="nav-wrapper">
+                <a href="#"><img class="brand-logo center" src="../res/logo.png"> </a>
             </div>
-            <div class="card-action">
-                <a href="#">This is a link</a>
-                <a href="#">This is a link</a>
+        </nav>
+    </div>
+
+    <div>
+        <nav class="z-depth-0 small-nav">
+            <div class="nav-wrapper">
+                <ul id="nav-mobile" class="centered-nav hide-on-med-and-down">
+                    <li><a href="home.php"><span class="material-icons">home</span> Home</a></li>
+                    <li><a class="dropdown-button" href="#!" data-activates="dropdown1"><span class="material-icons">location_on</span> Sensors Map<i class="material-icons right">arrow_drop_down</i></a></li>
+                    <li><a href="data-and-reports.html"><span class="material-icons">trending_up</span> Data and Reports</a></li>
+                    <li><a href="about.html"><span class="material-icons">email</span> About Us</a></li>
+                </ul>
             </div>
+        </nav>
+    </div>
+    <!-- Dropdown Structure -->
+    <ul id="dropdown1" class="dropdown-content">
+        <li><a id="drpBancal">SLEX - Carmona Exit</a></li>
+        <li><a id="drpSLEX">Bancal</a></li>
+    </ul>
+    <div>
+        <nav class="navbar-fixed">
+            <div class="nav-wrapper white">
+                <ul id="nav-mobile" class="centered-nav nav-status hide-on-med-and-down black-text">
+                    <li><a style="color: #2196F3;"><span class="material-icons">cloud</span> Good</a></li>
+                    <li><a style="color: #FFEB3B;"><span class="material-icons">cloud</span> Moderate</a></li>
+                    <li><a style="color: #FF9800;"><span class="material-icons">cloud</span> Unhealthy for Sensitive Groups</a></li>
+                    <li><a style="color: #f44336;"><span class="material-icons">cloud</span> Unhealthy</a></li>
+                    <li><a style="color: #9C27B0;"><span class="material-icons">cloud</span> Very Unhealthy</a></li>
+                    <li><a style="color: #b71c1c;"><span class="material-icons">cloud</span> Hazardous</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+    <div class="map-container">
+
+        <div class="card float-card">
+            <div class="card-content black-text">
+                <div class="row">
+                    <div id="AQIStat" class="col s12 m4">
+                        <span class="center-align"><h3>12345</h3></span>
+                    </div>
+                    <div class="col s12 m8">
+                        <div class="row">
+                            <div id="ZoneName" class="col s12">
+                                <span class="card-title"><b>Zone Name</b></span>
+                            </div>
+                            <div id="AQIStat_txt" class="col s12">
+                                <span>AQI Status</span>
+                            </div>
+                            <div id="timeUpdated" class="col s12">
+                                <span>DateToday TimeToday</span>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col s12 m12">
+                        <div class="carousel carousel-slider">
+                            <div class="carousel-item black-text" href="#one!">
+                                <table>
+                                    <thead>
+                                    <tr>
+                                        <th data-field="id">Current</th>
+                                        <th data-field="name">Min</th>
+                                        <th data-field="price">Max</th>
+                                    </tr>
+                                    </thead>
+
+                                    <tbody>
+                                    <tr>
+                                        <td>Alvin</td>
+                                        <td>Eclair</td>
+                                        <td>$0.87</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Alan</td>
+                                        <td>Jellybean</td>
+                                        <td>$3.76</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jonathan</td>
+                                        <td>Lollipop</td>
+                                        <td>$7.00</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="carousel-item black-text-text" href="#two!">
+                                <div class="row">
+                                    <div class="col s12 m12">
+                                        <p><h5>Synthesis</h5></p>
+                                    </div>
+                                    <div class="col s12 m12">
+                                        <p>The burning of fossil fuels to power industries and vehicles is a major cause of pollution.
+                                            Generating electrical power through thermal power stations releases huge amounts of carbon dioxide into the atmosphere.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="center-align">
+                    <a id ="prevItem" class="waves-effect waves-teal"><i class="material-icons">keyboard_arrow_left</i></a>
+                    <a id ="nextItem" class="waves-effect waves-teal"><i class="material-icons">keyboard_arrow_right</i></a>
+                </div>
+            </div>
+            <div id="plotOption">
+                <div class="divider"></div>
+                    <div class="card-action">
+                        <a href="#">AQI Plot</a>
+                        <a href="#">Concentration Plot</a>
+                    </div>
+            </div>
+            
         </div>
+
+        <div id="googleMap"></div>
     </div>
 </div>
-
-<div class="map-container">
-    <div id="googleMap"></div>
-</div>
-
 
 
 <!--  Scripts-->
