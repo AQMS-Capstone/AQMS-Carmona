3a69673e8084951f21539e8dfb806a746acc61be
Christian Que <vonnmesina@gmail.com>
2016-11-01 19:44:56 +0800

    preparation for db

---
+++ public/include/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Database config variables
+ */
+
+//ONLINE DB
+//define("DB_HOST", "MYSQL5016.myWindowsHosting.com");
+//define("DB_USER", "9fa58a_mtdb");
+//define("DB_PASSWORD", "melotrax123");
+//define("DB_DATABASE", "db_9fa58a_mtdb");
+
+//LOCAL DB
+define("DB_HOST", "localhost");
+define("DB_USER", "melotraxadmin");
+define("DB_PASSWORD", "m3lotraxpass1234");
+define("DB_DATABASE", "melotraxdb");
