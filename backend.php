aae2da63781f7d76a0b7442294f72d174713b6bd
Kris Mariano <renascence22@live.com>
2016-11-08 01:55:06 +0800

    kk

--- backend.php
+++ backend.php
@@ -4,32 +4,38 @@ require_once 'public/include/db_connect.php';
 
 if(isset($_POST['btnSubmit'])){
   $area = $_POST['area'];
-  $E_ID = $_POST['e_id'];
+  $E_ID = -1;
   $CValue = $_POST['cValue'];
   $time_now = "";
 
-  if($area == "" || $E_ID == "" || $CValue == ""){
+  if($area == "" || $E_ID == 0 || $CValue == ""){
     echo "Something's missing";
   }
   else{
+    $E_ID = $_POST['element'];
     $query = "SELECT timestamp  FROM MASTER WHERE E_ID = '$E_ID' and area_name='bancal' ORDER BY timestamp desc limit 1";
     $result = mysqli_query($con,$query);
 
     while($row = mysqli_fetch_array($result))
     {
       //$date_now = date("Y-m-d");
-        if(!empty($result)){
-            $time_now = date("Y-m-d H:i:s", strtotime($row['timestamp'])+3600);
+        if(mysql_num_rows($result)==0){
+            //$time_now = date("Y-m-d H:i:s", strtotime($row['timestamp'])+3600);
+            echo date("Y-m-d H:i:s", strtotime("00:00:00")+3600);
+            break;
         }
         else{
-          //echo "missing";
+          $time_now = date("Y-m-d H:i:s", strtotime($row['timestamp'])+3600);
           break;
         }
 
 
     }
+    echo $time_now;
 
-    /*
+    //echo date("Y-m-d H:00:00");
+
+/*
     $query = "INSERT INTO MASTER (m_id, area_name, e_id, concentration_value, timestamp) VALUES (NULL, '$area', '$E_ID', '$CValue', '$time_now')";
 
     if (!mysqli_query($con,$query))
@@ -41,7 +47,7 @@ if(isset($_POST['btnSubmit'])){
     {
       $statusMessage = "New record added successfully.";
     }
-    */
+*/
     mysqli_close($con);
     echo $time_now;
   }
@@ -59,7 +65,16 @@ if(isset($_POST['btnSubmit'])){
   <body>
     <form class="" action="" method="post">
       <input type="text" name="area" value="" placeholder="Area"><br><br>
-      <input type="text" name="e_id" value="" placeholder="Element ID"><br><br>
+      <select name = "element">
+          <option value="" disabled selected>Select an Element</option>
+          <option value="1">CO</option>
+          <option value="2">SO2</option>
+          <option value="3">NO2</option>
+          <option value="3">O3</option>
+          <option value="3">Pb</option>
+          <option value="3">PM 10</option>
+          <option value="3">TSP</option>
+      </select><br><br>
       <input type="text" name="cValue" value="" placeholder="Concentration Value"><br><br>
 
       <input type="submit" name="btnSubmit" value="Insert SQL">
