2404bcb472f67e5001c4ac5b7e777ea648e37be9
Lyle Vince Dela Cuesta <lylevincedelacuesta@gmail.com>
2016-11-09 02:42:23 +0800

    Tried to fix Backend.

--- backend.php
+++ backend.php
@@ -53,14 +53,14 @@ if(isset($_POST['btnSubmit'])){
 }
 
 ?>
-
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Air Quality Monitoring</title>
+    <title>Backend - Air Quality Monitoring</title>
     <link rel="icon" href="res/favicon.ico" type="image/x-icon" />
 
     <!-- CSS  -->
@@ -68,28 +68,66 @@ if(isset($_POST['btnSubmit'])){
     <link href="css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <link rel="icon" href="res/favicon.ico" type="image/x-icon" />
-  </head>
-  <body>
-    <form action="" method="post">
-      <input type="text" name="area" value="" placeholder="Area"><br><br>
-      <select name = "element">
-          <option value="" disabled selected>Select an Element</option>
-          <option value="1">CO</option>
-          <option value="2">SO2</option>
-          <option value="3">NO2</option>
-          <option value="3">O3</option>
-          <option value="3">Pb</option>
-          <option value="3">PM 10</option>
-          <option value="3">TSP</option>
-      </select><br><br>
-      <input type="text" name="cValue" value="" placeholder="Concentration Value"><br><br>
 
-      <input type="submit" name="btnSubmit" value="Insert SQL">
-    </form>
 
-    
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="js/materialize.min.js"></script>
-  <script src="js/init.js"></script>
-  </body>
+</head>
+
+<body>
+
+<div id="content-holder">
+    <div class="section">
+        <br><br>
+        <h2 class="header center teal-text"><b>Developer Option</b></h2>
+        <div class="row center">
+            <h6 class="header col s12">This page would act as a simulation of the IOT device</h6>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="section no-pad-bot">
+        <div class="container">
+            <div class="row">
+                <div class="col s12">
+                    <form>
+                        <div class="input-field col s6">
+                            <select id="pollutant">
+                                <option value="" disabled selected>Select a pollutant</option>
+                                <option value="1">CO</option>
+                                <option value="2">SO2</option>
+                                <option value="3">NO2</option>
+                                <option value="4">O3</option>
+                                <option value="5">Pb</option>
+                                <option value="6">PM10</option>
+                                <option value="7">SO2</option>
+                            </select>
+                            <label>Pollutant</label>
+                        </div>
+                        <div class="input-field col s2">
+                            <input id="concentration" type="number" class="validate" value="0">
+                            <label for="number">Concentration</label>
+                        </div>
+                        <div class="input-field col s2">
+                            <label id="unit">µg/m3</label>
+                        </div>
+                        <div class="input-field col s2">
+                            <button class="btn waves-effect waves-light" type="submit">Submit</button>
+                        </div>
+
+                    </form>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+</div>
+
+
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="js/materialize.min.js"></script>
+<script src="js/init.js"></script>
+
+</body>
 </html>
