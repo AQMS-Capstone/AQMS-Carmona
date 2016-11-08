96132ca11709bf3bc7ce62ab012f81d165621f8f
Lyle Vince Dela Cuesta <lylevincedelacuesta@gmail.com>
2016-11-08 22:41:36 +0800

    img func in daily.php

--- js/daily.js
+++ js/daily.js
@@ -34,71 +34,11 @@ $( document ).ready(function(){
 })
 
 function GetBancalStatus() {
-
     $("#zoneName").text('Bancal Carmona, Cavite');
-
-
+    $("#zoneImg").attr("src","res/images/area/bancal.jpg");
 }
-//
-// function GetSLEXStatus() {
-//     map.setZoom(16);
-//     map.setCenter(slexZoom);
-//
-//     $("#zoneStatus").show();
-//     document.getElementById("aqiText").style.color = slexAirQuality;
-//     document.getElementById("AQIStat").style.backgroundColor = slexAirQuality;
-//     document.getElementById("zoneName").innerHTML = 'SLEX Carmona Exit, Cavite';
-//     document.getElementById("prevalentPollutant").innerHTML = slexprevalentPollutant;
-//     document.getElementById("aqiNum").innerHTML = slexAQI;
-//     document.getElementById("aqiText").innerHTML = slexAQIStatus;
-//     document.getElementById("timeUpdated").innerHTML =  bancal_date_gathered;
-//
-//     //document.getElementById("e_symbol_1").innerHTML =  "HI";
-//     //document.getElementById("concentration_value_1").innerHTML =  "7";
-//
-//     //alert(bancal_aqi_values[0]);
-//
-//     for(var i = 0; i < bancal_aqi_values.length; i++)
-//     {
-//         var elementName = "e_symbol_" + (i+1);
-//         var conentrationName = "concentration_value_" + (i+1);
-//
-//         var elementMin = "aqi_min_" + (i+1);
-//         var elementMax = "aqi_max_" + (i+1);
-//
-//         document.getElementById(elementName).innerHTML =  pollutant_symbols[i];
-//         document.getElementById(conentrationName).innerHTML =  bancal_aqi_values[i];
-//
-//         document.getElementById(elementMin).innerHTML =  parseInt(JSON.stringify(bancal_min_max_values[i][0]).replace(/"/g, ''));
-//         document.getElementById(elementMax).innerHTML =  parseInt(JSON.stringify(bancal_min_max_values[i][1]).replace(/"/g, ''));
-//     }
-//
-//     /*
-//      for(var i = 0; i < slexValues_array.length; i++)
-//      {
-//      var elementName = "e_symbol_" + (i+1);
-//      var conentrationName = "concentration_value_" + (i+1);
-//
-//      document.getElementById(elementName).innerHTML =  slexValues_array[i].e_symbol;
-//      document.getElementById(conentrationName).innerHTML =  slexValues_array[i].concentration_value;
-//      }
-//      */
-//
-//     /*
-//      document.getElementById("e_symbol_1").innerHTML =  bancalValues_array[0].e_symbol;
-//      document.getElementById("e_symbol_2").innerHTML =  bancalValues_array[1].e_symbol;
-//      document.getElementById("e_symbol_3").innerHTML =  bancalValues_array[2].e_symbol;
-//      document.getElementById("e_symbol_4").innerHTML =  bancalValues_array[3].e_symbol;
-//      document.getElementById("e_symbol_5").innerHTML =  bancalValues_array[4].e_symbol;
-//      document.getElementById("e_symbol_6").innerHTML =  bancalValues_array[5].e_symbol;
-//      document.getElementById("e_symbol_7").innerHTML =  bancalValues_array[6].e_symbol;
-//
-//      document.getElementById("concentration_value_1").innerHTML =  bancalValues_array[0].e_symbol;
-//      document.getElementById("concentration_value_2").innerHTML =  bancalValues_array[1].e_symbol;
-//      document.getElementById("concentration_value_3").innerHTML =  bancalValues_array[2].e_symbol;
-//      document.getElementById("concentration_value_4").innerHTML =  bancalValues_array[3].e_symbol;
-//      document.getElementById("concentration_value_5").innerHTML =  bancalValues_array[4].e_symbol;
-//      document.getElementById("concentration_value_6").innerHTML =  bancalValues_array[5].e_symbol;
-//      document.getElementById("concentration_value_7").innerHTML =  bancalValues_array[6].e_symbol;
-//      */
-// }
+
+function GetSLEXStatus() {
+    $("#zoneName").text('SLEX Carmona Exit, Cavite');
+    $("#zoneImg").attr("src","res/images/area/slex_carmona-exit.jpg");
+}
