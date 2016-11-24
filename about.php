0b2b9779a6f318b76888a8ca6bdaa60646b96d68
Lyle Vince Dela Cuesta <lylevincedelacuesta@gmail.com>
2016-11-24 23:41:00 +0800

    Remove CGI Redirect

--- about.php
+++ about.php
@@ -16,7 +16,6 @@
 <html>
 <head>
     <meta name="generator" content="HTML Tidy for Linux (vers 25 March 2009), see www.w3.org">
-    <meta http-equiv="refresh" content="0;URL=/cgi-sys/defaultwebpage.cgi">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
