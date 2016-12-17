ae90caea7e48880887b806abb107c07f2511c578
Lyle Vince Dela Cuesta <lylevincedelacuesta@gmail.com>
2016-12-17 19:03:48 +0800

    INDEX LAYOUT INTEGRATED

---
+++ application/views/home.php
@@ -0,0 +1,13 @@
+<div id="googleMap" class="hide-on-med-and-down"></div>
+
+
+<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDNqg21fMXOnBCPajFuCDgy5zt6MkOPYv4"></script>
+<script src="https://cdn.rawgit.com/googlemaps/v3-utility-library/master/markerwithlabel/src/markerwithlabel.js"></script>
+<script src="<?php echo base_url("assets/js/caqms-api.js"); ?>"></script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script src="<?php echo base_url("assets/js/graph.js"); ?>"></script>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="<?php echo base_url("assets/js/materialize.js"); ?>"></script>
+<script src="<?php echo base_url("assets/js/init.js"); ?>"></script>
+</body>
+</html>
---
+++ application/views/shared/header.php
@@ -0,0 +1,111 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $title; ?></title>
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/materialize.min.css"); ?>" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>" rel="stylesheet" media="screen">
+
+    <link rel="icon" href="<?php echo base_url("assets/favicon.ico"); ?>" type="image/x-icon">
+
+    <meta property="og:url"                content="http://aqms.mcl-ccis.net/" />
+    <meta property="og:type"               content="website" />
+    <meta property="og:title"              content="Air Quality Monitoring System" />
+    <meta property="og:description"        content="The Air Quality Monitoring System (AQMS) promotes Air Pollution awareness and provides a unified Air Quality information for the municipality of Carmona. " />
+    <meta property="og:image"              content="<?php echo base_url("assets/facebook_og.jpg"); ?>" />
+</head>
+<body>
+
+<div>
+    <nav class="z-depth-1" style="height: 70px;">
+        <div class="nav-wrapper">
+            <a id="logo" href="index.php"><img class="brand-logo center" alt="Brand Logo" src="<?php echo base_url("assets/logo.png"); ?>"> </a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul class="side-nav" id="mobile-demo">
+                <li class="teal z-depth-1"><a id="logo" href="index.php"><img alt="Brand Logo" class="center"
+                                                                              src="<?php echo base_url("assets/logo.png"); ?>" style="height: 45px;">
+                    </a></li>
+                <li><a href="index.php" id="home-tab"><span class="material-icons">home</span> Home</a></li>
+                <li><a id="sensor"><span class="material-icons">location_on</span> Sensors Map<i
+                                class="material-icons right" style="margin-left: 5px!important;">arrow_drop_down</i></a>
+                </li>
+                <div hidden id="sensor-content">
+                    <li><a href="index.php?area=SLEX" id="drpSLEX">SLEX - Carmona Exit</a></li>
+                    <li><a href="index.php?area=Bancal" id="drpBancal">Bancal</a></li>
+                </div>
+
+                <li><a href="daily.php"><span class="material-icons">access_time</span> Daily</a></li>
+                <li><a href="history.php"><span class="material-icons">trending_up</span> History</a></li>
+                <li><a id="calculator"><span class="material-icons">timeline</span>Calculators<i
+                                class="material-icons right" style="margin-left: 5px!important;">arrow_drop_down</i></a>
+                </li>
+                <div hidden id="calculator-content">
+                    <li><a href="aqi-calculator.php?calculator=CVA" id="drpCVA">AQI Calculator</a></li>
+                    <li><a href="aqi-calculator.php?calculator=ACV" id="drpACV">Concentration Value Calculator</a></li>
+                </div>
+
+                <li><a href="about.php" id="about-tab"><span class="material-icons">email</span> About</a></li>
+            </ul>
+
+        </div>
+    </nav>
+</div>
+
+
+<div>
+    <nav id="nav" class="hide-on-med-and-down">
+        <div class="nav-wrapper">
+
+            <ul class="centered-nav">
+                <li><a href="index.php" id="home-tab"><span class="material-icons">home</span> Home</a></li>
+                <li><a class="dropdown-button" data-beloworigin="true" data-activates="dropdown1"><span
+                                class="material-icons">location_on</span> Sensors Map<i class="material-icons right"
+                                                                                        style="margin-left: 5px!important;">arrow_drop_down</i></a>
+                </li>
+                <li><a href="daily.php"><span class="material-icons">access_time</span> Daily</a></li>
+                <li><a href="history.php"><span class="material-icons">trending_up</span> History</a></li>
+                <li><a class="dropdown-button" data-beloworigin="true" data-activates="dropdown2"><span
+                                class="material-icons">timeline</span>Calculators<i class="material-icons right"
+                                                                                    style="margin-left: 5px!important;">arrow_drop_down</i></a>
+                </li>
+                <li><a href="about.php" id="about-tab"><span class="material-icons">email</span> About</a></li>
+
+            </ul>
+
+
+        </div>
+    </nav>
+</div>
+<!-- Dropdown Structure -->
+<ul id="dropdown2" class="dropdown-content">
+    <li><a href="aqi-calculator.php?calculator=CVA" id="drpCVA">AQI Calculator</a></li>
+    <li><a href="aqi-calculator.php?calculator=ACV" id="drpACV">Concentration Value Calculator</a></li>
+</ul>
+
+<ul id="dropdown1" class="dropdown-content">
+    <li><a href="index.php?area=SLEX" id="drpSLEX">SLEX - Carmona Exit</a></li>
+    <li><a href="index.php?area=Bancal" id="drpBancal">Bancal</a></li>
+</ul>
+
+<div id="legends">
+    <nav class="navbar-fixed hide-on-med-and-down">
+        <div class="nav-wrapper white">
+            <ul class="centered-legend">
+                <li style="color: #4CAF50;"><span class="material-icons">cloud</span> Good</li>
+                <li style="color: #FFEB3B;"><span class="material-icons">cloud</span> Fair</li>
+                <li style="color: #FF9800;"><span class="material-icons">cloud</span> Unhealthy for Sensitive Groups
+                </li>
+                <li style="color: #f44336;"><span class="material-icons">cloud</span> Very Unhealthy</li>
+                <li style="color: #9C27B0;"><span class="material-icons">cloud</span> Acutely Unhealthy</li>
+                <li style="color: #b71c1c;"><span class="material-icons">cloud</span> Emergency</li>
+            </ul>
+        </div>
+    </nav>
+</div>
