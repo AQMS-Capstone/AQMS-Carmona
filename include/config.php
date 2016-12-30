5c4fd79ea1b44d4c2078f07056dadd9afde70a0c
Lyle Vince Dela Cuesta <lylevincedelacuesta@gmail.com>
2016-12-30 18:16:43 +0800

    FUCKIN ONLINE DB

--- include/config.php
+++ include/config.php
@@ -6,9 +6,9 @@
 
 //ONLINE DB
 //define("DB_HOST", "localhost");
-//define("DB_USER", "mclccisn_aqmsph");
+//define("DB_USER", "mclccisn_aqms");
 //define("DB_PASSWORD", "AQMS@PH2016");
-//define("DB_DATABASE", "aqms");
+//define("DB_DATABASE", "mclccisn_aqms")
 
 //LOCAL DB
 define("DB_HOST", "localhost");
