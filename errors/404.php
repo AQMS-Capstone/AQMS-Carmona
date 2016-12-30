89ff2a346c4945f2a8ae7b7041dfe28af68ef6e0
Lyle Vince Dela Cuesta <lylevincedelacuesta@gmail.com>
2016-12-30 17:57:26 +0800

    added a new custom error page.

---
+++ errors/404.php
@@ -0,0 +1,31 @@
+<?php
+define('BASE_URL', 'http://localhost/aqms-carmona/');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>403 Forbidden</title>
+	<link rel="icon" href="<?php echo BASE_URL; ?>res/favicon.ico" type="image/x-icon"/>
+
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="<?php echo BASE_URL; ?>css/materialize.min.css" type="text/css" rel="stylesheet" media="screen">
+	<link href="<?php echo BASE_URL; ?>css/style.css" type="text/css" rel="stylesheet" media="screen">
+</head>
+<body>
+
+<div class="center-horizontal-vertical">
+	<div class="center-align">
+		<a id="logo" href="http://aqms.mcl-ccis.net/"><img class="center" style="width: 250px;height: 250px;" alt="Brand Logo" src="<?php echo BASE_URL; ?>res/Icon.png"> </a>
+		<br>
+		<br>
+		<h1 class="teal-text" style="margin: 0;">404</h1>
+		<h5>OOPS! Page not found. <a href="http://aqms.mcl-ccis.net/" class="orange-text">Go Back</a></h5>
+	</div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="<?php echo BASE_URL; ?>js/materialize.js"></script>
+<script src="<?php echo BASE_URL; ?>js/init.js"></script>
+</body>
+</html>
