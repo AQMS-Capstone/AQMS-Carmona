359c6b5e80662399aaecf1d303123f6fced75b85
Lyle Vince Dela Cuesta <lylevincedelacuesta@gmail.com>
2016-12-30 18:21:24 +0800

    Fix 403 page

--- errors/index.php
+++ errors/index.php
@@ -1,195 +1,31 @@
 <?php
-define("PAGE_TITLE", "Air Quality Monitoring System");
-include("include/Map.php");
-include('include/header.php');
+define('BASE_URL', 'http://localhost/aqms-carmona/');
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>403 Forbidden</title>
+	<link rel="icon" href="<?php echo BASE_URL; ?>res/favicon.ico" type="image/x-icon"/>
 
-<div id="content-holder">
-    <?php
-    $data = "";
-    if (isset($_GET["area"])) {
-        $data = $_GET["area"];
-    }
-    ?>
-    <div id="home">
-        <div id="googleMap" class="hide-on-med-and-down"></div>
-        <div class="container">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="<?php echo BASE_URL; ?>css/materialize.min.css" type="text/css" rel="stylesheet" media="screen">
+	<link href="<?php echo BASE_URL; ?>css/style.css" type="text/css" rel="stylesheet" media="screen">
+</head>
+<body>
 
-            <div id="zoneStatus" class="card float-card" hidden>
-                <div class="card-content black-text">
-                    <div class="row">
-                        <div class="col s12 m4">
-                            <div class="center-align">
-                                <p><span class="material-icons" style="font-size: 5em;" id="AQIStat">cloud</span></p>
-                                <p style="font-weight: bold; font-size: 1.5em;">AQI: <span id="aqiNum"></span></p>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="col s12 m7">
-                                <div class="row-no-after left-align-on-med-and-up center-align">
-                                    <div class="col s12">
-                                        <p style="font-weight: bold; font-size: 1.5em;" id="aqiText">AQI Status</p>
-                                        <span style="font-size: x-large"><b class="teal-text"
-                                                                            id="zoneName">Zone Name</b></span>
-                                    </div>
-                                    <div id="AQIStat_txt" class="col s12">
-                                        <b>Prevalent Air Pollutant: </b> <span id="prevalentPollutant">Prevalent Air Pollutant</span>
-                                    </div>
-                                    <div class="col s12">
-                                        <b>Recorded on: </b> <span id="timeUpdated">DateToday TimeToday</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div class="row">
-                        <div class="col s12 m12">
-                            <div class="hide-on-med-and-up center">
-                                <p class="orange-text center">Touch and Swipe for more info.</p>
-                                <br>
-                            </div>
-                            <div class="scroll">
-                                <table>
-                                    <thead>
-                                    <tr>
-                                        <th></th>
-                                        <th>Current</th>
-                                        <th></th>
-                                        <th>Min</th>
-                                        <th>Max</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-
-                                    <?php
-
-                                    function getAreaStatus($area_data)
-                                    {
-                                        $untilValue = $area_data->aqi_values;
-
-                                        if (count($area_data->AllDayValues_array) != 0) {
-                                            for ($x = 0; $x < count($untilValue); $x++) {
-                                                $maxValue = 0;
-
-                                                switch ($x) {
-                                                    case 0:
-                                                        $maxValue = $area_data->co_max;
-                                                        break;
-
-                                                    case 1:
-                                                        $maxValue = $area_data->so2_max;
-                                                        break;
-
-                                                    case 2:
-                                                        $maxValue = $area_data->no2_max;
-                                                        break;
-
-                                                    case 3:
-                                                        $maxValue = $area_data->o3_max;
-                                                        break;
-
-                                                    case 4:
-                                                        $maxValue = $area_data->pm10_max;
-                                                        break;
-
-                                                    case 5:
-                                                        $maxValue = $area_data->tsp_max;
-                                                        break;
-                                                }
-
-                                                if ($maxValue > -1) {
-                                                    $elementName = "e_symbol_" . ($x + 1);
-                                                    $conentrationName = "concentration_value_" . ($x + 1);
-                                                    $chartName = "chart_div_" . ($x + 1);
-                                                    $elementNameMin = "aqi_min_" . ($x + 1);
-                                                    $elementNameMax = "aqi_max_" . ($x + 1);
-
-                                                    echo "<tr>";
-                                                    echo "<td class='elementName' id='$elementName'>NaN</td>";
-                                                    echo "<td class='elementCurrent' id='$conentrationName'>NaN</td>";
-                                                    echo "<td><div class='chart'><canvas id='$chartName'></canvas></div></td>";
-                                                    echo "<td class='elementMin' id='$elementNameMin'>NaN</td>";
-                                                    echo "<td class='elementMax' id='$elementNameMax'>NaN</td>";
-                                                    echo "</tr>";
-                                                }
-                                            }
-                                        }
-                                    }
-
-                                    if (isset($_GET["area"])) {
-                                        $data = $_GET["area"];
-                                        $untilValue = array();
-
-                                        if ($data == "SLEX") {
-                                            getAreaStatus($slex);
-                                        } else if ($data == "Bancal") {
-                                            getAreaStatus($bancal);
-                                        }
-                                    }
-                                    ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div id="plotOption" class="center-align">
-                        <div class="divider"></div>
-                        <form action="daily.php" style="height: 100%;">
-                            <a class="waves-effect orange-text btn-flat" href="daily.php?area=<?php echo "$data" ?>"
-                               style="width:100%">VIEW MORE</a>
-                        </form>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
-
-    </div>
-
-    <div class="hide-on-med-and-up container">
-
-        <div class="row center card">
-            <div class="col s12 card-content">
-                <div>
-                    <a href="index.php?area=SLEX"><img class="img-circle img-small"
-                                                       src="res/images/area/slex_carmona-exit.jpg" alt="SLEX"></a>
-                </div>
-            </div>
-            <div class="col s12">
-                <h4>SLEX - Carmona, Exit</h4>
-            </div>
-        </div>
-
-        <div class="row center card">
-            <div class="col s12 card-content">
-                <div>
-                    <a href="index.php?area=Bancal"><img class="img-circle img-small" src="res/images/area/bancal.jpg"
-                                                         alt="Bancal"></a>
-                </div>
-            </div>
-            <div class="col s12">
-                <h4>Bancal</h4>
-            </div>
-        </div>
-
-    </div>
-
-
+<div class="center-horizontal-vertical">
+	<div class="center-align">
+		<a id="logo" href="http://aqms.mcl-ccis.net/"><img class="center" style="width: 250px;height: 250px;" alt="Brand Logo" src="<?php echo BASE_URL; ?>res/Icon.png"> </a>
+		<br>
+		<br>
+		<h1 class="teal-text" style="margin: 0;">403</h1>
+		<h5>Directory access is forbidden.  <a href="http://aqms.mcl-ccis.net/" class="orange-text">Go Back</a></h5>
+	</div>
 </div>
 
 
-<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDNqg21fMXOnBCPajFuCDgy5zt6MkOPYv4"></script>
-<script src="https://cdn.rawgit.com/googlemaps/v3-utility-library/master/markerwithlabel/src/markerwithlabel.js"></script>
-<script src="js/caqms-api.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.js"></script>
-<script src="js/graph.js"></script>
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="js/materialize.js"></script>
-<script src="js/init.js"></script>
-
+<script src="<?php echo BASE_URL; ?>js/materialize.js"></script>
+<script src="<?php echo BASE_URL; ?>js/init.js"></script>
 </body>
 </html>
