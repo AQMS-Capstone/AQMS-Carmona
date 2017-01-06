cbc7d228cbabd8e21ab3aed193ead22b1a9d884d
Lyle Vince Dela Cuesta <lylevincedelacuesta@gmail.com>
2017-01-07 02:29:46 +0800

    Change Graph sample to 12 hr but we will be using 24 hr
    For demonstration purpose

--- js/sample-graph.js
+++ js/sample-graph.js
@@ -25,21 +25,38 @@ var ctx_bar = document.getElementById("barChart");
 var barChart = new Chart(ctx_bar, {
     type: 'bar',
     data: {
-        labels: ["NO2", "CO2", "SO2"],
-        datasets: [{
-            data: [102, 329, 243],
-            backgroundColor: [
-                '#FF9800',
-                '#3F51B5',
-                '#E91E63',
-            ]
-        }]
+        labels: ["01", "02", "03","04", "05", "06","07", "08", "09", "10", "11", "12"],
+        datasets: [
+            {
+                label:"NO2",
+                backgroundColor: "#FF9800",
+                data: [3,7,4,3,7,4,3,7,4,3,7,4]
+            },
+            {
+                label:"CO2",
+                backgroundColor: "#3F51B5",
+                data: [4,3,5,4,3,5,4,3,5,4,3,5]
+            },
+            {
+                label:"SO2",
+                backgroundColor: "#E91E63",
+                data: [7,2,6,7,2,6,7,2,6,7,2,6]
+            }
+        ]
     },
     options:{
         maintainAspectRatio: false,
         responsive: true,
         legend:{
-            display: false
+            display: true
+        },
+        scales: {
+            xAxes: [{
+                stacked: true,
+            }],
+            yAxes: [{
+                stacked: true
+            }]
         }
     }
 });
