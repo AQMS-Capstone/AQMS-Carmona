4806a4f2fc3342564e98e45c69429c5d9dd250ae
Christian Que <vonnmesina@gmail.com>
2017-01-07 19:40:49 +0800

    Same color of elements for the two graphs

--- js/sample-graph.js
+++ js/sample-graph.js
@@ -47,19 +47,19 @@ function drawTheGraph2(area_data, rolling_time) {
             labels: [area_data.rolling_time[0].toString(), rolling_time[1].toString(), rolling_time[2].toString(), rolling_time[3].toString(), rolling_time[4].toString(), rolling_time[5].toString(), rolling_time[6].toString(), rolling_time[7].toString(), rolling_time[8].toString(), rolling_time[9].toString(), rolling_time[10].toString(), rolling_time[11].toString(), rolling_time[12].toString(), rolling_time[13].toString(), rolling_time[14].toString(), rolling_time[15].toString(), rolling_time[16].toString(), rolling_time[17].toString(), rolling_time[18].toString(), rolling_time[19].toString(), rolling_time[20].toString(), rolling_time[21].toString(), rolling_time[22].toString(), rolling_time[23].toString()],
             datasets: [
                 {
-                    label: "NO2",
+                    label: pollutant_symbols[0],
                     backgroundColor: "#FF9800",
-                    data: [checkData(area_data.no2_aqi_values[0]), checkData(area_data.no2_aqi_values[1]), checkData(area_data.no2_aqi_values[2]), checkData(area_data.no2_aqi_values[3]), checkData(area_data.no2_aqi_values[4]), checkData(area_data.no2_aqi_values[5]), checkData(area_data.no2_aqi_values[6]), checkData(area_data.no2_aqi_values[7]), checkData(area_data.no2_aqi_values[8]), checkData(area_data.no2_aqi_values[9]), checkData(area_data.no2_aqi_values[10]), checkData(area_data.no2_aqi_values[11]), checkData(area_data.no2_aqi_values[12]), checkData(area_data.no2_aqi_values[13]), checkData(area_data.no2_aqi_values[14]), checkData(area_data.no2_aqi_values[15]), checkData(area_data.no2_aqi_values[16]), checkData(area_data.no2_aqi_values[17]), checkData(area_data.no2_aqi_values[18]), checkData(area_data.no2_aqi_values[19]), checkData(area_data.no2_aqi_values[20]), checkData(area_data.no2_aqi_values[21]), checkData(area_data.no2_aqi_values[22]), checkData(area_data.no2_aqi_values[23])]
+                    data: checkIndex(pollutant_symbols[0], area_data)
                 },
                 {
-                    label: "CO2",
+                    label: pollutant_symbols[1],
                     backgroundColor: "#3F51B5",
-                    data: [checkData(area_data.co_aqi_values[0]), checkData(area_data.co_aqi_values[1]), checkData(area_data.co_aqi_values[2]), checkData(area_data.co_aqi_values[3]), checkData(area_data.co_aqi_values[4]), checkData(area_data.co_aqi_values[5]), checkData(area_data.co_aqi_values[6]), checkData(area_data.co_aqi_values[7]), checkData(area_data.co_aqi_values[8]), checkData(area_data.co_aqi_values[9]), checkData(area_data.co_aqi_values[10]), checkData(area_data.co_aqi_values[11]), checkData(area_data.co_aqi_values[12]), checkData(area_data.co_aqi_values[13]), checkData(area_data.co_aqi_values[14]), checkData(area_data.co_aqi_values[15]), checkData(area_data.co_aqi_values[16]), checkData(area_data.co_aqi_values[17]), checkData(area_data.co_aqi_values[18]), checkData(area_data.co_aqi_values[19]), checkData(area_data.co_aqi_values[20]), checkData(area_data.co_aqi_values[21]), checkData(area_data.co_aqi_values[22]), checkData(area_data.co_aqi_values[23])]
+                    data: checkIndex(pollutant_symbols[1], area_data)
                 },
                 {
-                    label: "SO2",
+                    label: pollutant_symbols[2],
                     backgroundColor: "#E91E63",
-                    data: [checkData(area_data.so2_aqi_values[0]), checkData(area_data.so2_aqi_values[1]), checkData(area_data.so2_aqi_values[2]), checkData(area_data.so2_aqi_values[3]), checkData(area_data.so2_aqi_values[4]), checkData(area_data.so2_aqi_values[5]), checkData(area_data.so2_aqi_values[6]), checkData(area_data.so2_aqi_values[7]), checkData(area_data.so2_aqi_values[8]), checkData(area_data.so2_aqi_values[9]), checkData(area_data.so2_aqi_values[10]), checkData(area_data.so2_aqi_values[11]), checkData(area_data.so2_aqi_values[12]), checkData(area_data.so2_aqi_values[13]), checkData(area_data.so2_aqi_values[14]), checkData(area_data.so2_aqi_values[15]), checkData(area_data.so2_aqi_values[16]), checkData(area_data.so2_aqi_values[17]), checkData(area_data.so2_aqi_values[18]), checkData(area_data.so2_aqi_values[19]), checkData(area_data.so2_aqi_values[20]), checkData(area_data.so2_aqi_values[21]), checkData(area_data.so2_aqi_values[22]), checkData(area_data.so2_aqi_values[23])]
+                    data: checkIndex(pollutant_symbols[2], area_data)
                 }
             ]
         },
@@ -79,4 +79,19 @@ function drawTheGraph2(area_data, rolling_time) {
             }
         }
     });
+}
+
+function checkIndex(pollutant, area_data){
+
+    var arrayContainer = [];
+
+    if(pollutant == "CO"){
+        arrayContainer = [checkData(area_data.co_aqi_values[0]), checkData(area_data.co_aqi_values[1]), checkData(area_data.co_aqi_values[2]), checkData(area_data.co_aqi_values[3]), checkData(area_data.co_aqi_values[4]), checkData(area_data.co_aqi_values[5]), checkData(area_data.co_aqi_values[6]), checkData(area_data.co_aqi_values[7]), checkData(area_data.co_aqi_values[8]), checkData(area_data.co_aqi_values[9]), checkData(area_data.co_aqi_values[10]), checkData(area_data.co_aqi_values[11]), checkData(area_data.co_aqi_values[12]), checkData(area_data.co_aqi_values[13]), checkData(area_data.co_aqi_values[14]), checkData(area_data.co_aqi_values[15]), checkData(area_data.co_aqi_values[16]), checkData(area_data.co_aqi_values[17]), checkData(area_data.co_aqi_values[18]), checkData(area_data.co_aqi_values[19]), checkData(area_data.co_aqi_values[20]), checkData(area_data.co_aqi_values[21]), checkData(area_data.co_aqi_values[22]), checkData(area_data.co_aqi_values[23])];
+    }else if(pollutant == "SO2"){
+        arrayContainer = [checkData(area_data.so2_aqi_values[0]), checkData(area_data.so2_aqi_values[1]), checkData(area_data.so2_aqi_values[2]), checkData(area_data.so2_aqi_values[3]), checkData(area_data.so2_aqi_values[4]), checkData(area_data.so2_aqi_values[5]), checkData(area_data.so2_aqi_values[6]), checkData(area_data.so2_aqi_values[7]), checkData(area_data.so2_aqi_values[8]), checkData(area_data.so2_aqi_values[9]), checkData(area_data.so2_aqi_values[10]), checkData(area_data.so2_aqi_values[11]), checkData(area_data.so2_aqi_values[12]), checkData(area_data.so2_aqi_values[13]), checkData(area_data.so2_aqi_values[14]), checkData(area_data.so2_aqi_values[15]), checkData(area_data.so2_aqi_values[16]), checkData(area_data.so2_aqi_values[17]), checkData(area_data.so2_aqi_values[18]), checkData(area_data.so2_aqi_values[19]), checkData(area_data.so2_aqi_values[20]), checkData(area_data.so2_aqi_values[21]), checkData(area_data.so2_aqi_values[22]), checkData(area_data.so2_aqi_values[23])];
+    }else if(pollutant == "NO2"){
+        arrayContainer = [checkData(area_data.no2_aqi_values[0]), checkData(area_data.no2_aqi_values[1]), checkData(area_data.no2_aqi_values[2]), checkData(area_data.no2_aqi_values[3]), checkData(area_data.no2_aqi_values[4]), checkData(area_data.no2_aqi_values[5]), checkData(area_data.no2_aqi_values[6]), checkData(area_data.no2_aqi_values[7]), checkData(area_data.no2_aqi_values[8]), checkData(area_data.no2_aqi_values[9]), checkData(area_data.no2_aqi_values[10]), checkData(area_data.no2_aqi_values[11]), checkData(area_data.no2_aqi_values[12]), checkData(area_data.no2_aqi_values[13]), checkData(area_data.no2_aqi_values[14]), checkData(area_data.no2_aqi_values[15]), checkData(area_data.no2_aqi_values[16]), checkData(area_data.no2_aqi_values[17]), checkData(area_data.no2_aqi_values[18]), checkData(area_data.no2_aqi_values[19]), checkData(area_data.no2_aqi_values[20]), checkData(area_data.no2_aqi_values[21]), checkData(area_data.no2_aqi_values[22]), checkData(area_data.no2_aqi_values[23])];
+    }
+
+    return arrayContainer;
 }
