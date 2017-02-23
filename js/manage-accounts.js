5b7f07f48d3de92274c7049a7a7ed5300d12f160
Lyle Vince Dela Cuesta <lylevincedelacuesta@gmail.com>
2017-02-23 13:10:33 +0800

    Partially fix device status and time (not realtime) :-1:

--- js/manage-accounts.js
+++ js/manage-accounts.js
@@ -43,3 +43,47 @@ $(document).ready(function () {
 $(document).on('click', function () {
     $('.modal-trigger').leanModal({});
 });
+
+$.ajax({
+    type: "GET",
+    url: 'retrieve_device_status.php',
+    dataType: 'JSON',
+    success: function (response) {
+        var status1 = response["status1"];
+        var status2 = response["status2"];
+
+        if(status1 == "1"){
+            $('#status1').html("SENDING");
+            $('#status1').attr("class","green-text");
+        }else{
+            $('#status1').html("DISCONNECTED");
+            $('#status1').attr("class","red-text");
+        }
+
+        if(status2 == "1"){
+            $('#status2').html("SENDING");
+            $('#status2').attr("class","green-text");
+        }else{
+            $('#status2').html("DISCONNECTED");
+            $('#status2').attr("class","red-text");
+        }
+    }
+});
+$(function()
+{
+    myGetFeed = setTimeout('GetServerTime()', 1000);
+});
+
+function GetServerTime()
+{
+    $.ajax({
+        type: "GET",
+        url: 'retrieve_time.php',
+        dataType: 'JSON',
+        success: function (response) {
+            var time = response["serverTime"];
+            $('#serverTime').html(time);
+        }
+    });
+
+}
